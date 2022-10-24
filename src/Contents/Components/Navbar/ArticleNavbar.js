2332c0050910d1cd0462169888c8aacf5d594ee8
adam <adamharits25@gmail.com>
2022-10-24 07:28:57 +0700

    RumahHypno 2.1

--- src/Contents/Components/Navbar/ArticleNavbar.js
+++ src/Contents/Components/Navbar/ArticleNavbar.js
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import logo from "../../Attachments/Images/RumahHypnoLogo.png";
-import { HiChevronDown } from "react-icons/hi";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
--- src/Contents/Sections/Articles.js
+++ src/Contents/Sections/Articles.js
@@ -1,15 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import ArticleNavbar from "../Components/Navbar/ArticleNavbar";
 import Section1 from "./ArticlePages/Section1";
-import Section2 from "./ArticlePages/Section2";
-import Section3 from "./ArticlePages/Section3";
-import Section4 from "./ArticlePages/Section4";
-import Section5 from "./ArticlePages/Section5";
-import Section6 from "./ArticlePages/Section6";
-import Section7 from "./ArticlePages/Section7";
-import Section8 from "./ArticlePages/Section8";
-import Section9 from "./ArticlePages/Section9";
-import Section10 from "./ArticlePages/Section10";
+
 
 import PostsPg from "./PostsPg";
 import ArticleFooter from "../Components/Footer/ArticleFooter";
